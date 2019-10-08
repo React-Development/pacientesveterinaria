4d61178c912f892c78586835227482c010c52fe8
bgarcia95 <bgarcia.95@outlook.com>
2019-10-07 22:47:35 -0600

    Eliminando citas

--- src/App.js
+++ src/App.js
@@ -2,17 +2,42 @@ import React, { Component } from "react";
 import "./bootstrap.min.css";
 import Header from "./components/Header";
 import NuevaCita from "./components/NuevaCita";
+import ListaCitas from "./components/ListaCitas";
 
 class App extends Component {
   state = {
     citas: []
   };
 
+  // Cuando laaplicacion carga
+  componentDidMount() {
+
+  }
+
+  // Cuando eliminamos o agregamos una nueva cita
+  // localStorage no soporta arreglos y es por eso que convertimos nuestro arreglo de citas a  String
+  componentDidUpdate() {
+    localStorage.setItem('citas', JSON.stringify(this.state.citas));
+  }
+
   crearNuevaCita = datos => {
     //copiar el state actual
     const citas = [...this.state.citas, datos];
 
     // agregar el nuevo state
+    this.setState({ citas });
+  };
+
+  // elimina las citas del state
+  eliminarCita = id => {
+    // console.log(id);
+    // console.log('diste click');
+
+    // tomar copia del state
+    const citasActuales = [...this.state.citas];
+    // utilizar filter para sacar el elemento @id del arreglo
+    const citas = citasActuales.filter(cita => cita.id !== id);
+    // actualizar el state
     this.setState({ citas });
   };
 
@@ -24,6 +49,13 @@ class App extends Component {
         <div className="row">
           <div className="col-md-10 mx-auto">
             <NuevaCita crearNuevaCita={this.crearNuevaCita} />
+          </div>
+
+          <div className="mt-5 col-md-10 mx-auto">
+            <ListaCitas
+              citas={this.state.citas}
+              eliminarCita={this.eliminarCita}
+            />
           </div>
         </div>
       </div>
